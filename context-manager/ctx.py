1696652eb371fefc243f0008b22fc1b5388e9138
Felix Stephen <felsen88@gmail.com>
2017-08-09 18:02:54 +0530

    Modified: changes done for ctx

--- context-manager/ctx.py
+++ context-manager/ctx.py
@@ -14,16 +14,28 @@ finally:
 
 """
 
+
+class temptables:
+
+    def __init__(self, cur):
+        self.cur = cur
+
+    def __enter__(self, ):
+        cur.execute("create table points(x int, y int)")
+
+    def __exit__(self, *args):
+        cur.execute("drop table points")
+
+
 with connect("test.db") as conn:
     cur = conn.cursor()
-    cur.execute("create table points(x int, y int)")
-    cur.execute("insert into points(x, y) values(1, 2)")
-    cur.execute("insert into points(x, y) values(1, 4)")
-    cur.execute("insert into points(x, y) values(3, 2)")
-    cur.execute("insert into points(x, y) values(5, 2)")
-    for row in cur.execute("select x, y from points"):
-        print(row)
-    for row in cur.execute("select sum(x * y) from points"):
-        print(row)
-    cur.execute("drop table points")
+    with temptables(cur):
+        cur.execute("insert into points(x, y) values(1, 2)")
+        cur.execute("insert into points(x, y) values(1, 4)")
+        cur.execute("insert into points(x, y) values(3, 2)")
+        cur.execute("insert into points(x, y) values(5, 2)")
+        for row in cur.execute("select x, y from points"):
+            print(row)
+        for row in cur.execute("select sum(x * y) from points"):
+            print(row)
 
